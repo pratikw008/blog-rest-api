aaf34fe1f80d038c7703cbe5d4d4aaad7e89ab87
pratikw008 <pratikwankhede008@gmail.com>
2023-10-31 18:28:33 +0530

    initail commit

---
+++ src/test/java/com/app/dev/blog/BlogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.dev.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
